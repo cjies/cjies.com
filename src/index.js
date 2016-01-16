2edda2a85c1b1f830497357c43e32147cb0108e9
cjies <cijies@gmail.com>
2016-01-16 22:54:35 +0800

    update travis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App.js';
 
 // Redux
 import { createStore } from 'redux';
