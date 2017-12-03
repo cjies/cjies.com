c3995b6f04c1e24e4341e45f76edb92a4738f8e9
CJies <cijies@gmail.com>
2017-12-03 12:43:31 +0800

    [eslint] Install prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'; 
 import logo from './logo.svg';
 import './App.css';
-
-const Button = styled.button`
-  color:#fff;
-  background-color: ${({ dark }) => dark ? '#aaa' : '#000'};
-`;
 
 class App extends Component {
   render() {
@@ -19,8 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button dark>asd</Button>
-        <Button>asds</Button>
       </div>
     );
   }
