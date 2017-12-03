3b0f2a13249e83c13d603051fb720945b6f65e2a
CJies <cijies@gmail.com>
2017-12-03 18:36:12 +0800

    [index] Add banner on console

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
-
 import registerServiceWorker from './registerServiceWorker';
+
+// Behind the Scenes
+// import { repository, author, version } from '../package.json';
+// import banner from './banner';
+
+// console.info(`
+//   ${banner}
+//   I <3 React~~~ Ｏ(≧▽≦)Ｏ
+//   Page version: v${version}
+//   Behind the scenes: ${repository.url}
+//   Hiring me? ${author.email}
+// `);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
