35c2270c7b3327186b4ec1a5d3019c20d980799c
CJies <cijies@gmail.com>
2018-01-25 14:13:24 +0800

    [about_me] Apply github's avatar

--- src/data/about_me.js
+++ src/data/about_me.js
@@ -1,4 +1,4 @@
-export const AVATAR = '//graph.facebook.com/1089854948/picture?type=large';
+export const AVATAR = '//avatars1.githubusercontent.com/u/5558360?s=460';
 
 export const PARAGRAPH = `
 Hello. My name is CJies Tan. I am the front-end developer from Malaysia and staying in Taipei now. And I also the Web Designer and EDM lover 📻 💛 I love to make things simple. All I require is a mac, a book, and a pencil to occupy me all day. Ya! Of course, and a coffee too. Nice to meet you.
