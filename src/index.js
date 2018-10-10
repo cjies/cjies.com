855fce1f68fcd900d5e8226d6f622dcae2304b57
CJies Tan <cijies@gmail.com>
2018-10-10 15:05:01 +0800

    Print banner in console

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Behind the Scenes
-// import { repository, author, version } from '../package.json';
-// import CONSOLE_BANNER from './data/console_banner';
+import { repository, author, version } from '../package.json';
+import CONSOLE_BANNER from './data/console_banner';
 
-// console.info(`
-//   ${CONSOLE_BANNER}
-//   I <3 React~~~ Ｏ(≧▽≦)Ｏ
-//   Page version: v${version}
-//   Behind the scenes: ${repository.url}
-//   Hiring me? ${author.email}
-// `);
+console.info(`
+  ${CONSOLE_BANNER}
+  I <3 React~~~ Ｏ(≧▽≦)Ｏ
+  Page version: v${version}
+  Behind the scenes: ${repository.url}
+  Hiring me? ${author.email}
+`);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
