81a78f44bfadee67a53fc48c536cbe8875c50d0e
CJies Tan <cijies@gmail.com>
2018-10-10 15:18:13 +0800

    Staff cleanup

--- src/data/about_me.js
+++
@@ -1,8 +0,0 @@
-export const AVATAR = '//avatars1.githubusercontent.com/u/5558360?s=460';
-
-export const PARAGRAPH = `
-Hello. My name is CJies Tan. I am the front-end developer from Malaysia and staying in Taipei now. And I also the Web Designer and EDM lover 📻 💛 I love to make things simple. All I require is a mac, a book, and a pencil to occupy me all day. Ya! Of course, and a coffee too. Nice to meet you.
-`;
-
-// For contact section
-export const MY_EMAIL = 'cijies@gmail.com';
