2b7a58523a7ed66725521ec17f8f30b46bec9d77
CJies Tan <cijies@gmail.com>
2019-07-09 22:31:24 +0800

    Update work experiences

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,11 +1,11 @@
 // @flow
 export default [
   {
-    title: 'Front End Engineer',
+    title: 'Senior Front End Engineer',
     unit: 'Appier',
     location: 'Taipei, Taiwan',
     duration: '2017 - Present',
-    description: 'Aixon Team',
+    description: 'Enterprise Solution Department',
   },
   {
     title: 'F2E&RGBA Meetup',
@@ -26,13 +26,6 @@ export default [
     unit: 'Galaxy Software Services',
     location: 'Taipei, Taiwan',
     duration: '2014 - 2016',
-    description: 'Front End Developer & Web Designer',
-  },
-  {
-    title: 'Bachelor of Computer Science',
-    unit: 'National Cheng Kung University',
-    location: 'Tainan, Taiwan',
-    duration: '2010 - 2014',
-    description: 'Major in web development',
+    description: 'Uwillx Team',
   },
 ];
