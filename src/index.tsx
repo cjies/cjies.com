0287be50c229156525229a7563dbbf7923304679
CJ Tan <cj.tan@onedegree.hk>
2021-05-07 08:27:19 +0800

    refactor: remove serviceworker and setupTests files, but add web vitals report

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import App from './App/index';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 // Behind the Scenes
 import { repository, author, version } from '../package.json';
@@ -23,7 +23,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
