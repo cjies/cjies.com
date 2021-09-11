c88838d88e70a28c9f01d08e0ae8d7ab3d57e994
cj.tan <xi-jie.tan@houzz.com>
2021-09-11 12:25:10 +0800

    feat: update about me

--- src/data/about_me.ts
+++ src/data/about_me.ts
@@ -3,9 +3,9 @@ export const AVATAR = '//avatars1.githubusercontent.com/u/5558360?s=460';
 const expDuration = new Date().getFullYear() - 2014;
 
 export const PARAGRAPH = `
-Hello, my name is CJies Tan. I am a software engineer and web developer
+Hello, my name is CJ Tan. I am a software engineer and web developer
 with ${expDuration}+ years of experience from Malaysia, but based in Taipei.
-Specialize in frontend development with react/redux, angularJS and hybrid application.
+Specialize in web development with react/redux, Django, PHP and hybrid application.
 Love to write code in high-paced and challenging environment to deliver high-quality and maintainable product.
 `;
 
